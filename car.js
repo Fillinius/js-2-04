0d7c46f83102e434fed0dc118f5c7bd294d81e39
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-08 13:07:29 +0300

    Add files via upload

---
+++ car.js
@@ -0,0 +1,27 @@
+const carName = 'VW'
+
+let maxCarSpeed = '165km/h'
+console.log('Скорость машины по умолчанию', maxCarSpeed);
+maxCarSpeed = '200km/h'
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+maxCarSpeed = '220km/h'
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+maxCarSpeed = '225km/h'
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+
+let carOwner = 'Vladimir'
+console.log('Первый владелец машины', carOwner);
+carOwner = 'Natalia'
+console.log('У машины изменился владелец', carOwner)
+
+carOwner = 'Vlad'
+console.log('У машины изменился владелец', carOwner)
+
+carOwner = 'Nata'
+console.log('У машины изменился владелец', carOwner)
+
+
+
